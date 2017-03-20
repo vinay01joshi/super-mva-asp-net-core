ba38f1d6b4e5df47288a0790aeb0ca504d10003f
Vinay Joshi <vinay01joshi@gmail.com>
2017-03-20 20:48:49 +0530

    Adding package in Asp.net core project and tooling such as watcher in the project

--- startup.cs
+++ startup.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,7 +9,7 @@ namespace supermva
         public void Configure(IApplicationBuilder app)
         {
             app.Run(context =>{
-                return context.Response.WriteAsync("Hello from web!");
+                return context.Response.WriteAsync("Hello Everyone!");
             });
         }
     }
